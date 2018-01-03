52465e639fbb0ef3b1facf32a9efe7827436d1fa
Matt Lally <mattlally@Matts-MacBook-Pro.local>
2018-01-03 13:29:36 -0500

    Added some comments about modding transfer functions to eliminate trading before the token sale ends.

--- pitchToken.sol
+++ pitchToken.sol
@@ -49,6 +49,11 @@ contract StandardToken is Token {
         //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
         //Replace the if with this one instead.
         //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        
+        // TODO: If token sale is not finished.
+        // AND user is not the owner of the contract
+        // don't allow transfer
+
         if (balances[msg.sender] >= _value && _value > 0) {
             balances[msg.sender] -= _value;
             balances[_to] += _value;
@@ -60,6 +65,11 @@ contract StandardToken is Token {
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         //same as above. Replace this line with the following if you want to protect against wrapping uints.
         //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        
+        // TODO: If token sale is not finished.
+        // AND user is not the owner of the contract
+        // don't allow transfer
+
         if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
             balances[_to] += _value;
             balances[_from] -= _value;
