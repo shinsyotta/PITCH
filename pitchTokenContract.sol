bef89814f32207698ceb3dc0373c25869983e4d1
Matt Lally <mattlally@Matts-MacBook-Pro.local>
2018-01-03 13:47:49 -0500

    Clarified a comment.

--- pitchTokenContract.sol
+++ pitchTokenContract.sol
@@ -9,7 +9,7 @@ contract ERC20Token {
 
 // Consider using SafeMath library from OpenZeppelin.
 contract PricedTokenSale {
-    // PMKN hardcoded for simplicity
+    // PMKN token hardcoded for simplicity
     address tokenAddress = 0xcD7f46B8A66203B842c7B68863de7e90643E426B; // Whatever is the contract address for pitchToken when deployed.
     
     // Need to accept payment and transfer it to custody of another wallet.
